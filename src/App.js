9a512d2c8eaa0dd5727779084acacf7513f04c8c
mike-dalton <michael.j.dalton@gmail.com>
2023-07-13 18:58:23 -0400

    rework chriscourse without redux for fun

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useRef } from 'react'
 
 function App() {
+
+  const [state, setState] = useState({
+    posts: [],
+    title: ''
+  });
+
+  const inputElement = useRef();
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(json => {
+      setState(prev => ({ ...prev, posts: json}));
+    })
+  }, []);
+
+  const displayPosts = state.posts.map(post => {
+    return (
+      <li key={post.id}>{post.title}</li>
+    );
+  }).reverse();
+
+  const getNextId = function() {
+    return state.posts.reduce((max, currentvalue) => {
+      return currentvalue.id > max ? currentvalue.id : max;
+    }, 0) + 1;
+  };
+
+  const handleChange = function(title) {
+    setState(prev => ({ ...prev, title}));
+  };
+
+  const addPost = function(event) {
+    event.preventDefault();
+    setState(prev => ({ ...prev, posts: [...state.posts, { id: getNextId(), title: state.title}] }));
+    setState(prev => ({ ...prev, title: ''}));
+    inputElement.current.focus();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +57,16 @@ function App() {
         >
           Learn React
         </a>
+        <input
+          type='text'
+          placeholder='Type title here'
+          ref={inputElement}
+          value={state.title}
+          onChange={event => handleChange(event.target.value)}
+        >
+        </input>
+        <button type='submit' onClick={addPost}>Add Post</button>
+        <ul>{displayPosts}</ul>
       </header>
     </div>
   );
